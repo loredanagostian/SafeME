0cacd574f2daf33272898710d3ce6337cfaefcfe
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-11-11 17:44:09 +0200

    added default sizes

---
+++ lib/constants/sizes.dart
@@ -0,0 +1,6 @@
+class AppSizes {
+  static const double marginSize = 20.0;
+  static const double smallDistance = 8.0;
+  static const double mediumDistance = 16.0;
+  static const double bigDistance = 32.0;
+}
