99a05e8d7778857ed7dbf9929c92d5478f441f04
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-11-11 17:29:06 +0200

    added default colors and google png

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkGray = Color(0x00464555);
+  static const Color mediumGray = Color(0x00aba9bb);
+  static const Color lightGray = Color(0x00ececec);
+  static const Color componentGray = Color(0x00f5f5f5);
+  static const Color mainRed = Color(0x00ed0038);
+  static const Color mainBlue = Color(0x002764ff);
+  static const Color lightBlue = Color(0x003c96ff);
+  static const Color white = Colors.white;
+  static const Color mainGreen = Color(0x004caf50);
+  static const Color lightGreen = Color(0x0060d365);
+}
