3ace160be7ad547b761b1df420a9e14079738ba8
Lore Gostian <loredana.gostian@cobaltsign.com>
2023-11-12 21:58:22 +0200

    updates colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color darkGray = Color(0x00464555);
-  static const Color mediumGray = Color(0x00aba9bb);
-  static const Color lightGray = Color(0x00ececec);
-  static const Color componentGray = Color(0x00f5f5f5);
-  static const Color mainRed = Color(0x00ed0038);
-  static const Color mainBlue = Color(0x002764ff);
-  static const Color lightBlue = Color(0x003c96ff);
+  static const Color darkGray = Color.fromARGB(255, 70, 69, 85);
+  static const Color mediumGray = Color.fromARGB(255, 171, 169, 187);
+  static const Color lightGray = Color.fromARGB(255, 236, 236, 236);
+  static const Color componentGray = Color.fromARGB(255, 245, 245, 245);
+  static const Color mainRed = Color.fromARGB(255, 237, 0, 55);
+  static const Color mainBlue = Color.fromARGB(255, 39, 100, 255);
+  static const Color lightBlue = Color.fromARGB(255, 60, 151, 255);
   static const Color white = Colors.white;
-  static const Color mainGreen = Color(0x004caf50);
-  static const Color lightGreen = Color(0x0060d365);
+  static const Color mainGreen = Color.fromARGB(255, 76, 175, 79);
+  static const Color lightGreen = Color.fromARGB(255, 96, 211, 102);
 }
