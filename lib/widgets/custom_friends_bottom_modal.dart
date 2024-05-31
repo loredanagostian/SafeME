71655524ea57481a36188cefb3ebdd86ab080757
Lore Gostian <loredana.gostian@cobaltsign.com>
2024-05-31 23:18:17 +0300

    added local datas

--- lib/widgets/custom_friends_bottom_modal.dart
+++ lib/widgets/custom_friends_bottom_modal.dart
@@ -1,48 +1,71 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:safe_me/constants/colors.dart';
 import 'package:safe_me/constants/strings.dart';
+import 'package:safe_me/managers/firebase_manager.dart';
+import 'package:safe_me/managers/user_info_provider.dart';
 import 'package:safe_me/models/account.dart';
+import 'package:safe_me/models/user_static_data.dart';
 import 'package:safe_me/widgets/custom_list_tile.dart';
 
-class CustomFriendsBottomModal extends StatelessWidget {
-  final List<Account> allFriends;
-  final Account userAccount;
-  const CustomFriendsBottomModal({
-    super.key,
-    required this.allFriends,
-    required this.userAccount,
-  });
+class CustomFriendsBottomModal extends ConsumerStatefulWidget {
+  const CustomFriendsBottomModal({super.key});
+
+  @override
+  ConsumerState<CustomFriendsBottomModal> createState() =>
+      _CustomFriendsBottomModalState();
+}
+
+class _CustomFriendsBottomModalState
+    extends ConsumerState<CustomFriendsBottomModal> {
+  late UserStaticData _userStaticData;
+
+  @override
+  void initState() {
+    super.initState();
+    _userStaticData = ref.read(userStaticDataProvider);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: allFriends.length,
-      itemBuilder: (context, index) {
-        final item = allFriends[index];
+    _userStaticData = ref.watch(userStaticDataProvider);
 
-        return CustomListTile(
-          photoUrl: item.imageURL,
-          title: item.firstName,
-          subtitle: item.phoneNumber,
-          buttonText: AppStrings.addButton,
-          button1Action: () {
-            userAccount.emergencyContacts.contains(item.userId)
-                ? null
-                : FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(userAccount.userId)
-                    .update({
-                    "emergencyContacts": FieldValue.arrayUnion([item.userId]),
-                  }).then((value) => Navigator.pop(context));
-          },
-          isAlreadyFriend: userAccount.emergencyContacts.contains(item.userId),
-          buttonColor: userAccount.emergencyContacts.contains(item.userId)
-              ? AppColors.mediumGray
-              : AppColors.mainBlue,
-        );
-      },
+    return ListView.builder(
       shrinkWrap: true,
+      itemCount: _userStaticData.friends.length,
+      itemBuilder: (context, index) {
+        final friendId = _userStaticData.friends[index];
+
+        return FutureBuilder(
+            future: FirebaseManager.fetchUserInfo(friendId),
+            builder: (context, snapshot) {
+              if (snapshot.hasData &&
+                  snapshot.connectionState == ConnectionState.done) {
+                Account friendItem = snapshot.data!;
+
+                return CustomListTile(
+                  photoUrl: friendItem.imageURL,
+                  title: friendItem.firstName,
+                  subtitle: friendItem.phoneNumber,
+                  buttonText: AppStrings.addButton,
+                  button1Action: () {
+                    _userStaticData.emergencyContacts
+                            .contains(friendItem.userId)
+                        ? null
+                        : FirebaseManager.addEmergencyContact(friendItem.userId)
+                            .then((value) => Navigator.pop(context));
+                  },
+                  isAlreadyFriend: _userStaticData.emergencyContacts
+                      .contains(friendItem.userId),
+                  buttonColor: _userStaticData.emergencyContacts
+                          .contains(friendItem.userId)
+                      ? AppColors.mediumGray
+                      : AppColors.mainBlue,
+                );
+              }
+              return Container();
+            });
+      },
     );
   }
 }
